2579bb3c71b1a1722c2683f3396af1fac9de0725
Jacob <Jacobzeme8@gmail.com>
2023-03-31 10:45:46 -0600

    init

---
+++ keeper.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'jacobzeme8.us.auth0.com'
+export const clientId = 'K799U2HHVsa9CjMS7abRMinjFBb3IPV9'
+export const audience = 'https://codeworks-yakub-api.com'
